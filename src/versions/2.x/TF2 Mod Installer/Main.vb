ad167b183ef2733912e68b8c366d2ee0fdda3432
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2020-11-28 16:21:10 -0500

    Started work on main app

--- src/versions/2.x/TF2 Mod Installer/Main.vb
+++ src/versions/2.x/TF2 Mod Installer/Main.vb
@@ -1,6 +1,11 @@
-﻿Module Main
+﻿Imports System.IO
+Imports System.Windows.Forms
+
+
+Module Main
 
     Sub Main()
+        Console.ForegroundColor = ConsoleColor.Green
         Console.Title = "Team Fortress 2 Mod Installer"
         Console.WriteLine("===================================")
         Console.WriteLine("= Team Fortress 2 Mod Installer   =")
@@ -12,12 +17,31 @@
     End Sub
 
     Sub ModInstaller()
-
+        Console.WriteLine("Loading...")
+        Dim preferredName As String = My.Settings.PersonalName
+        Dim customFolder As String = My.Settings.CustomFolder
+        Dim tf2InstallPath As String = My.Settings.tf2InstallPath
+        Dim backupMCS As Boolean = My.Settings.BackupMyCustomStuff
+        Dim steam As String = My.Settings.SteamFolder
+        Dim keeptemp As Boolean = My.Settings.KeepTempFiles
+        Console.Clear()
+        Console.Title = "Team Fortress 2 Mod Installer"
+        Console.WriteLine("===================================")
+        Console.WriteLine("= Team Fortress 2 Mod Installer   =")
+        Console.WriteLine("===================================")
+        Console.WriteLine("= GNU GPL V3.0                    =")
+        Console.WriteLine("= Copyright © TheCrafters001 2020 =")
+        Console.WriteLine("===================================")
+        Console.WriteLine("Welcome, " + preferredName)
+        Console.WriteLine("Press ENTER to continue.")
+        Console.ReadKey(Keys.Enter)
     End Sub
 
     Sub Checker()
         If My.Settings.IsFirstTime = True Then
             setup.PreSetup()
+            Console.Title = "Team Fortress 2 Mod Installer"
+            ModInstaller()
         ElseIf My.Settings.IsFirstTime = False Then
             ModInstaller()
         End If
