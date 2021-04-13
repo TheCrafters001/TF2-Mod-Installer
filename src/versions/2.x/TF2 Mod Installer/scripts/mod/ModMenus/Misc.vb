5827aa4e0b8bbf82888b1c0db1902d022d73afea
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2021-04-13 09:11:24 -0400

    code spaghetti

--- src/versions/2.x/TF2 Mod Installer/scripts/mod/ModMenus/Misc.vb
+++ src/versions/2.x/TF2 Mod Installer/scripts/mod/ModMenus/Misc.vb
@@ -1,6 +1,7 @@
 ﻿Imports System.Windows.Forms
 Imports ModDownload
 Imports UnRAR_Handler
+Imports _7zhandler
 
 Public Class MiscMenu
     Public Shared Sub Misc(ByVal IsUninstall As Boolean)
@@ -35,11 +36,31 @@ Public Class MiscMenu
 
         If choice = 0 Then
             modSelecter.ModMenu(IsUninstall)
+        ElseIf choice = 1 Then
+            Try
+                If IsUninstall = False Then
+                    Downloader.DownloadNow("TDE.zip", "https://files.gamebanana.com/skins/tiny_desk_engineers.zip", "Tiny Desk Engie Over Intel")
+                    extract_install.ExtractNow("TDE.zip", "Tiny Desk Engie Over Intel", My.Settings.CustomFolder)
+                    tempFileCleaner.Init()
+                    Console.WriteLine("Done installing mod.")
+                    Console.WriteLine("Press ENTER to continue.")
+                    Console.ReadKey(Keys.Enter)
+                    Main.Main()
+                ElseIf IsUninstall = True Then
+                    modUninstaller.UninstallFolder(My.Settings.CustomFolder & "\ahud-master", "ahud")
+                    Console.WriteLine("Done removing mod.")
+                    Console.WriteLine("Press ENTER to continue.")
+                    Console.ReadKey(Keys.Enter)
+                    Main.Main()
+                End If
+            Catch ex As Exception
+                errorHandler.errorHandler(ex.ToString, "Failed to Install/Uninstall Mod", "TF2MI-Error-00007", "Something went wrong, and the mod could not be installed/uninstalled." + vbCrLf + "Please press enter to recover.")
+            End Try
         ElseIf choice = 4 Then
             Try
                 If IsUninstall = False Then
                     Downloader.DownloadNow("jjba_killsound.rar", "https://files.gamebanana.com/sounds/retired_killsound_9ce09.rar", "JJBA ""RETIRED"" Killsound")
-                    extract_NoInstall.ExtractNow("jjba_killsound.rar", "JJBA RETIRED Killsound", My.Settings.CustomFolder)
+                    UnRAR_Handler.extract_NoInstall.ExtractNow("jjba_killsound.rar", "JJBA RETIRED Killsound", My.Settings.CustomFolder)
                     sndModInstall.Install("JJBA ""RETIRED"" Killsound", "JJBA RETIRED Killsound\RETIRED! Killsound\ui", "ui")
                     tempFileCleaner.Init()
                     Console.WriteLine("Done installing mod.")
